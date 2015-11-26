888eea4575da81a47afc644e5d50763113e568f7
jmcintosh <jmcintosh4s@gmail.com>
2015-11-25 22:53:32 -0700

    grabbing weather data, and a poster

--- windrose.R
+++ windrose.R
@@ -1,6 +1,10 @@
 # www.openair-project.org
 #install.packages("openair")
-#library(openair)
+library(openair)
+
+# http://ram-n.github.io/weatherData/
+# install.packages("weatherData")
+library(weatherData)
 
 airport <- read.csv("Butte_2Winters/Airport_hourly.csv")
 
@@ -14,7 +18,7 @@ names(airport)[names(airport)=="temp.degC."] <- "tempC"
 #airport <- airport[complete.cases(airport),]
 
 # convert string to posix timestamp
-airport$date <- as.POSIXct(airport$date)
+airport$date <- as.POSIXct(airport$date, tz = "MST", format="%Y-%m-%d %H:%M:%S")
 
 windRose(airport,pollutant = "pm25", paddle = FALSE, angle = 22.5, breaks = c(0,5,10,15,20))
 smoothTrend(airport,pollutant = "pm25", deseason = TRUE, statistic = "percentile", percentile = c(25,50,75,95), type = "wd")
@@ -22,3 +26,35 @@ calendarPlot(airport,pollutant = "pm25",year=2014)
 #mydata <- mydata
 
 plot(x=airport$tempC,y=airport$pm25)
+
+airport.start.date <- min(airport$date)
+airport.end.date <- max(airport$date)
+
+# butte airport code: KBTM
+
+showAvailableColumns("KBTM","2012-10-01",opt_detailed =T)
+weather <- getWeatherForDate("KBTM", 
+                  start_date = toString(airport.start.date),
+                  end_date = toString(airport.end.date),
+                  opt_detailed = TRUE,
+                  opt_write_to_file = FALSE,
+                  opt_all_columns = TRUE
+                  )
+
+getNearestWeatherData <- function (timestamp, x=weather$Time){
+  index <- which(abs(x-timestamp) == min(abs(x-timestamp)))[[1]]
+  weather[index,c(3:7,9,11,13,14)]
+}
+
+cToF <- function(tempC){
+  tempC*1.8 + 32
+}
+
+
+
+airport.weather <- as.data.frame(t(sapply(airport$date+30*60,getNearestWeatherData)))
+
+airport$TemperatureF <- unlist(airport.weather$TemperatureF)
+airport$Humidity <- unlist(airport.weather$Humidity)
+
+plot(airport$tempC,airport$TemperatureF)
